9eafcf8c6a48169956a6ab5840d18498694b0119
materubag <mrubio7466@uta.edu.ec>
2025-04-23 14:25:11 -0500

    Agregando Backend y bd.php

---
+++ BackEnd/bd.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'drivego';
+$user = 'root';
+$password = '';
+try {
+    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+
+?>
