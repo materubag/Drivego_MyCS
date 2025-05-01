8ccc602c0bfc1413447ea678f1847bebee4bf796
ArteMisa82 <anavero162@gmail.com>
2025-04-30 23:05:12 -0500

    Agregación: Se agrego la información a los archivos Footer.jsx y Navbar.jsx VJ

--- FrontEnd/src/Components/Footer.jsx
+++ FrontEnd/src/Components/Footer.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
-
+import React from 'react'
 const Footer = () => {
-    return (
-        <div>
-            <h1>Footer</h1>
-        </div>
-    );
-};
+  return (
+   <footer className='footer'>
+    <a href="/" className="logo">
+        <img src="/Logo-sin_fodo.png" alt='logo de la empresa'/> 
+      </a>
+      <div className='ubicacion'>
+       <a>
+        Pagina web dedicada a la venta de carros 
+       </a>
+       <a>
+        Ubicacion: Uta
+       </a>
+      </div>
+      <div className="social-media"> 
+        <a className='let'>
+            Redes Sociales
+        </a>
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa-brands fa-facebook"></i>
+          Facebook
+        </a>
+        <a href="https://WhatsApp.com" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-whatsapp"></i>
+          Whatsapp
+        </a>
+        <a
+          href="https://www.instagram.com/veru7xd?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa-brands fa-instagram"></i>
+          Instagram
+        </a>
+      </div>
+      <p className='derechos'>© 2025 DriveGo. All Rights Reserved.</p> 
+   </footer>
+    
+  )
+}
 
-export default Footer;
+export default Footer
