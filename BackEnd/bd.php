1eef9b7b8c03861c81e65cb1483f42e7182517da
Ismael <Jcuadrado1092@uta.edu.ec>
2025-05-01 17:07:04 -0500

    Configuracion de mi propia base

--- BackEnd/bd.php
+++ BackEnd/bd.php
@@ -1,13 +1,13 @@
 <?php
 $host = 'localhost';
-$dbname = 'drivego';
-$user = 'root';
-$password = '';
+$dbname = 'DB_Driver';
+$user = 'postgres';
+$password = 'DriverG@';
 try {
     $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa a la base de datos PostgreSQL.";
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
-
 ?>
