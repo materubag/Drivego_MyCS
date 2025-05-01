12687c961852ccb14906fb631123a87d69a60e97
materubag <mrubio7466@uta.edu.ec>
2025-05-01 17:14:07 -0500

    db

--- BackEnd/bd.php
+++ BackEnd/bd.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $dbname = 'drivego';
-$user = 'root';
-$password = '';
+$user = 'admin';
+$password = 'admin';
 try {
     $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
