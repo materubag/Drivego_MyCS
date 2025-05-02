1bc9a4920efe2024fa5280eebd25eb8370ea43ca
Ismael <Jcuadrado1092@uta.edu.ec>
2025-05-01 21:52:55 -0500

    Se corrijio errores en el modal del login

--- FrontEnd/src/Components/Navbar.jsx
+++ FrontEnd/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Modal from "../Modals/Login";
+import ModalLogin from "../Modals/Login";
 import ModalUser from "../Modals/User";
 
 const linksCliente = [
